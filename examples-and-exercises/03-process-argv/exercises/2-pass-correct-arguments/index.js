c25cd4023fa543d57fd369880e78f91bea7ad9f4
samirm00 <sammoham.be@gmail.com>
2021-03-02 12:12:59 +0100

    2-pass-correct-arguments, remove the line at the end

--- examples-and-exercises/03-process-argv/exercises/2-pass-correct-arguments/index.js
+++ examples-and-exercises/03-process-argv/exercises/2-pass-correct-arguments/index.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 // hint: how can you pass an argument with a space in it?
 
-const { it } = require('../../../describe-it.js');
-const assert = require('assert');
+const { it } = require("../../../describe-it.js");
+const assert = require("assert");
 
-const listify = require('./listify.js');
+const listify = require("./listify.js");
 
 const userInput = process.argv.slice(2);
 
@@ -13,14 +13,10 @@ const listifiedInput = listify(userInput);
 
 console.log(listifiedInput);
 
-it('listified input should be correct', () => {
+it("listified input should be correct", () => {
   const expected = `- white cow
 - red cape
 - yellow hair
-- gold slipper
-`;
+- gold slipper`;
   assert.strictEqual(listifiedInput, expected);
 });
-
-
-
